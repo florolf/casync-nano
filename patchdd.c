c7d0986a04befe7c52d45ef2a8e39922a3aa7069
Florian Larysch <florian.larysch@grandcentrix.net>
2019-11-20 19:35:06 +0100

    patchdd: show progress indicator when running interactively

--- patchdd.c
+++ patchdd.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -19,6 +20,19 @@ static size_t memcmp_offset(uint8_t *a, uint8_t *b, size_t len)
 			break;
 
 	return offset;
+}
+
+static void progress(int percent)
+{
+	static size_t calls = 0;
+	static const char spinner[] = "-\\|//";
+
+	printf("\033[0G[%c] %3d%%", spinner[calls++ % strlen(spinner)], percent);
+
+	if (percent == 100)
+		putchar('\n');
+
+	fflush(stdout);
 }
 
 int main(int argc, char **argv)
@@ -64,11 +78,12 @@ int main(int argc, char **argv)
 	uint8_t src_buf[TRANSFER_SIZE];
 	uint8_t dst_buf[TRANSFER_SIZE];
 
-	off_t src_remaining = src_size;
+	bool print_progress = isatty(STDOUT_FILENO);
+
 	off_t offset = 0;
 	off_t total_skipped = 0;
-	while (src_remaining) {
-		size_t to_read = MIN(src_remaining, TRANSFER_SIZE);
+	while (offset < src_size) {
+		size_t to_read = MIN(src_size - offset, TRANSFER_SIZE);
 
 		if (preadall(src, src_buf, to_read, offset) < 0)
 			return EXIT_FAILURE;
@@ -83,12 +98,14 @@ int main(int argc, char **argv)
 			return EXIT_FAILURE;
 
 		offset += to_read;
-		src_remaining -= to_read;
+
+		if (print_progress)
+			progress((100ull * offset) / src_size);
 	}
 
 	now = time_monotonic();
-	u_log(INFO, "synchronization finished after %u seconds", (unsigned int)(now - start));
-	u_log(INFO, "skipped a total of %llu bytes (%.2f%% of the input)",
+	printf("synchronization finished after %u seconds\n", (unsigned int)(now - start));
+	printf("skipped a total of %llu bytes (%.2f%% of the input)\n",
 	      (unsigned long long)total_skipped,
 	      (double)((100.0 * total_skipped)/src_size));
 
