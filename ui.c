db762f0827676f4eacf7fd6eb47b03ed3e9792c4
Florian Larysch <florian.larysch@grandcentrix.net>
2019-11-26 14:43:10 +0100

    ui: display graphical progress bar

--- ui.c
+++ ui.c
@@ -6,13 +6,22 @@
 
 #include "ui.h"
 
+#define PROGRESS_BAR_LEN 60
+
 void show_progress(uint8_t percent, progess_status_t *status)
 {
 	static const char spinner[] = "-\\|//";
+	char bar[PROGRESS_BAR_LEN + 1];
 
 	u_assert(status);
 
-	printf("\033[0G[%c] %3d%%", spinner[(*status)++ % strlen(spinner)], percent);
+	size_t tip = percent * PROGRESS_BAR_LEN / 100;
+	for (size_t i = 0; i < PROGRESS_BAR_LEN; i++)
+		bar[i] = (i < tip) ? '=' : ' ';
+
+	bar[PROGRESS_BAR_LEN] = 0;
+
+	printf("\033[0G[%c] |%s| %3d%%", spinner[(*status)++ % strlen(spinner)], bar, percent);
 
 	if (percent == 100)
 		putchar('\n');
