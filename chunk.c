5554c603d758d25653f14bad3754261ca7d39bd0
Florian Larysch <fl@n621.de>
2022-11-21 13:17:17 +0100

    chunk: constify arguments

--- chunk.c
+++ chunk.c
@@ -6,7 +6,7 @@
 
 #include "chunk.h"
 
-void chunk_format_id(char *id_str, uint8_t *id)
+void chunk_format_id(char *id_str, const uint8_t *id)
 {
 	for (size_t i = 0; i < CHUNK_ID_LEN; i++)
 		u_assert_se(sprintf(&id_str[2*i], "%02x", id[i]) == 2);
--- chunk.h
+++ chunk.h
@@ -9,9 +9,9 @@
 #define CHUNK_ID_LEN 32
 #define CHUNK_ID_STRLEN (2 * (CHUNK_ID_LEN) + 1)
 
-void chunk_format_id(char *id_str, uint8_t *id);
+void chunk_format_id(char *id_str, const uint8_t *id);
 
-static inline void chunk_calculate_id(uint8_t *data, size_t len, uint8_t *id_out)
+static inline void chunk_calculate_id(const uint8_t *data, size_t len, uint8_t *id_out)
 {
 	u_assert_se(sha_once(data, len, id_out) == 0);
 }
